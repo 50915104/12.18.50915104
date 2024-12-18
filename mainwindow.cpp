611fd55091fc5afad90cc8c54b31c4943c28a2ea
李瑋澄 <50915104@gm.nfu.edu.tw>
2024-12-18 21:48:21 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,8 +9,6 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    // Connect save button to the saveToFile slot
     connect(ui->action_5, &QAction::triggered, this, &MainWindow::saveToFile);
 }
 
@@ -21,21 +19,21 @@ MainWindow::~MainWindow()
 
 void MainWindow::saveToFile()
 {
-    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Text Files (*.txt);;All Files (*)"));
+    QString fileName = QFileDialog::getSaveFileName(this, tr("存檔"), "", tr("Text Files (*.txt);;All Files (*)"));
 
     if (fileName.isEmpty()) {
-        return; // User canceled the save dialog
+        return;
     }
 
     QFile file(fileName);
     if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-        QMessageBox::warning(this, tr("Error"), tr("Cannot open file for writing: %1").arg(file.errorString()));
+        QMessageBox::warning(this, tr("錯誤"), tr("無法打開檔案: %1").arg(file.errorString()));
         return;
     }
 
     QTextStream out(&file);
-    out << ui->textEdit->toPlainText(); // Save the content of textEdit
+    out << ui->textEdit->toPlainText();
     file.close();
 
-    QMessageBox::information(this, tr("Success"), tr("File saved successfully!"));
+    QMessageBox::information(this, tr("成功"), tr("檔案儲存成功!"));
 }
